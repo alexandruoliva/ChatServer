95957c4495c48d6b3a5f9531ac60dabed0d95584
OLIVA Alexandru-Florin <olivaalexandru@yahoo.com>
2018-09-18 16:45:38 +0300

    refactored the ServiceGui

--- src/ServerGui.java
+++ src/ServerGui.java
@@ -21,22 +21,52 @@ import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 
 public class ServerGui extends JPanel  {
-	JPanel chatClient = new JPanel();
-	JTextField inputTextTab = new JTextField("server input message",50);
-	JTextArea outputTextTab = new JTextArea(5, 5);
-	GridBagConstraints gridBagCon = new GridBagConstraints();
+	JPanel chatClient ;
+	JTextField inputTextTab ;
+	JTextArea outputTextTab ;
+	GridBagConstraints gridBagCon ;
 
-	private ObjectOutputStream output;
-	private ObjectInputStream input;
-	private ServerSocket serverSocket;
-	private Socket socket;
-	private static final int PORT = 6789;
+	public JTextField getInputTextTab() {
+		return inputTextTab;
+	}
 
 	
+	public void setInputTextTab(JTextField inputTextTab) {
+		this.inputTextTab = inputTextTab;
+	}	
 	
-	
-	
+
+	public JTextArea getOutputTextTab() {
+		return outputTextTab;
+	}
+
+
+	public void setOutputTextTab(JTextArea outputTextTab) {
+		this.outputTextTab = outputTextTab;
+	}
+
+
 	public ServerGui() {
+		initGuiElements();
+
+		inputTextTab.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				// getActionCommand sends the message;
+				sendMessage(event.getActionCommand());
+				outputTextTab.setText("");
+
+			}
+		});
+
+	}
+
+
+	private void initGuiElements() {
+		chatClient = new JPanel();
+		 inputTextTab = new JTextField("server input message",50);
+		 outputTextTab = new JTextArea(5, 5);
+		gridBagCon = new GridBagConstraints();
+		
 		gridBagCon.weightx = 0.5;
 		gridBagCon.weighty = 1.0;
 		gridBagCon.fill = GridBagConstraints.BOTH;
@@ -58,151 +88,9 @@ public class ServerGui extends JPanel  {
 		// we don't want to let the users to type while are disconnected
 		inputTextTab.setEditable(false);
 		add(inputTextTab, gridBagCon);
-
-		inputTextTab.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent event) {
-				// getActionCommand sends the message;
-				sendMessage(event.getActionCommand());
-				outputTextTab.setText("");
-
-			}
-		});
-
 	}
 
-	// set up and run server
-	public void startRunning() {
-		serverSocket =null;
-		socket=null;
-		
-		try {
-			serverSocket =new ServerSocket(PORT,100);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		try {
-			// 6789 port number
-			while (true) {
-				try {
-
-					waitForConnection();
-
-					setupStreams();
-
-					whileChatting();
-				} catch (EOFException eofException) {
-					showMessage("\n Server ended the connection!");
-				} finally {
-					closeChat();
-				}
-				Thread t= new ClientHandler(output, input, socket);
-				t.start();
-			}
-		} catch (IOException ioException) {
-			ioException.printStackTrace();
-		}
-	}
-
-	// wait for connection, the display connection information
-	private void waitForConnection() throws IOException {
-		int counter =0;
-		showMessage("Waiting for someone to connect....\n");
-
-		// listens to the connection and accepts it
-		socket = serverSocket.accept();
-		counter ++;
-		showMessage("Client number "+counter +" has connected \n");
-		
-		showMessage("Now connected to " + socket.getInetAddress().getHostName());
-		
 	
-	}
-
-	// get stream to send and receive data
-	private void setupStreams() throws IOException {
-		
-		
-		// creating the pathway that allows us to connect to another computer
-		output = new ObjectOutputStream(socket.getOutputStream());
-		// flushes the data bytes that are leftover into the stream
-		output.flush();
-		input = new ObjectInputStream(socket.getInputStream());
-		showMessage("\n Streams are now setup! \n");
-		
-	
-		
-
-	}
-
-	// during the chat conversation
-	private void whileChatting() throws IOException {
-		String message = "You are now connected ! ";
-		sendMessage(message);
-		ableToType(true);
-		do {
-
-			try {
-				message = (String) input.readObject();
-				showMessage("\n" + message);
-			} catch (ClassNotFoundException classNotFoundException) {
-				showMessage("\n idk what that user sent ");
-			}
-		} while (!message.equals("CLIENT - END"));// if the users type END the
-	}
-
-	// close streams and sockets after you are done chatting
-	private void closeChat() {
-		showMessage("\n Closing connection...");
-		ableToType(false);
-		try {
-			output.close();
-			input.close();
-			socket.close();
-		} catch (IOException ioException) {
-			ioException.printStackTrace();
-		}
-
-	}
-
-	// send a message to client
-	private void sendMessage(String message) {
-		try {
-			output.writeObject("SERVER - " + message);
-			output.flush();
-			showMessage("\nSERVER -" + message);
-		} catch (IOException ioException) {
-			outputTextTab.append("\n ERROR: CANT'T SEND THAT MESSAGE");
-		}
-		inputTextTab.setText(null);
-		
-	}
-
-	// updates chatWindow
-	private void showMessage(final String text) {
-		SwingUtilities.invokeLater(new Runnable() {
-			public void run() {
-				outputTextTab.append(text);
-			}
-		});
-	}
-	private void showMessage(final String text,int counter) {
-		SwingUtilities.invokeLater(new Runnable() {
-			public void run() {
-				outputTextTab.append(text);
-			}
-		});
-	}
-
-	// let the user type stuff into their box
-	private void ableToType(final boolean trueOrFalse) {
-		SwingUtilities.invokeLater(new Runnable() {
-			public void run() {
-				inputTextTab.setText("");
-				inputTextTab.setEditable(trueOrFalse);
-			}
-		});
-	}
-
 	public void buildFrame(ImageIcon icon, ServerGui object, JFrame frame, String titleFrame) {
 		frame.setTitle(titleFrame);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
